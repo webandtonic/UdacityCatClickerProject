a279286d028ccc851b59c265ac8adf4a15b23f8e
Kerstin Hofmann <kerstinhofmann@Kerstins-iMac.fritz.box>
2019-02-05 22:15:34 +0100

    added click event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+document.addEventListener("DOMContentLoaded", function () {
+
+	function addEvtLst() {
+		let catImage = document.querySelector("img");
+		catImage.addEventListener("click", countClick);
+		console.log(catImage);
+	};
+
+	function countClick() {
+		console.log("clicked!");
+	};
+
+	addEvtLst();
+
+});
