08bf10e6c4da38d58fbc85caea2d94925c973ec0
Kerstin Hofmann <kerstinhofmann@Kerstins-iMac.fritz.box>
2019-02-06 20:06:26 +0100

    added gulp, sass, browsersync to the project

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+
+gulp.task('default', ['styles'], function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+  browserSync.init({
+		server: './'
+  });
+});
+
+gulp.task('styles', function() {
+  gulp
+    .src('sass/**/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('./styles'))
+    .pipe(browserSync.stream());
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,41 @@
 // JavaScript Document
 document.addEventListener("DOMContentLoaded", function () {
-
 	let clicked = 0;
-	let counter = document.querySelector("span");
+	let counter = document.querySelectorAll("span");
+	let catImages = document.querySelectorAll("img");
 	let cats = [];
 
-	function NewCat(name, id) {
+	function NewCat(name, i) {
 		this.name = name;
-		this.url = "images/cat-" + id;
+		this.url = "images/cat-0" + i + ".jpg";
+		this.clicked = 0;
 	};
 
 	function addEvtLst() {
-		let catImages = document.querySelectorAll("img");
 		for (let catImage of catImages) {
 			catImage.addEventListener("click", countClick);
 		};
 	};
 
+
+
 	function countClick() {
 		clicked += 1;
-		counter.innerHTML = (clicked);
+		document.querySelector("span." + this.className).innerHTML = (clicked);
+	};
+
+
+	let cat1 = new NewCat("Tiffy", 1);
+	cats.push(cat1);
+	let cat2 = new NewCat("Fluffy", 2);
+	cats.push(cat2);
+
+	for (i = 0; i < 2; i++) {
+		counter[i].className = cats[i].name;
+		catImages[i].className = cats[i].name;
+		catImages[i].src = cats[i].url;
 	};
 
 	addEvtLst();
-
-	let cat1 = new NewCat("Tiffy", "01");
-	cats.push(cat1);
-	let cat2 = new NewCat("Fluffy", "02");
-	cats.push(cat2);
-
-	console.log(cat1);
-	console.log(cat2);
-	console.log(cats);
 
 });
