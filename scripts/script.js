81103638389444b68a73f4731bf1cc5ffcc2cb30
Kerstin Hofmann <kerstinhofmann@Kerstins-iMac.fritz.box>
2019-02-06 21:26:23 +0100

    new project req 3: updated NewCat function and created 5 cats

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,47 @@
 // JavaScript Document
 document.addEventListener("DOMContentLoaded", function () {
-	let clicked = 0;
-	let counter = document.querySelectorAll("span");
-	let catImages = document.querySelectorAll("img");
-	let cats = [];
+	// let clicked = 0;
+	// let counter = document.querySelectorAll("span");
+	// let catImages = document.querySelectorAll("img");
+	let cats = ["Tiffy", "Fluffy", "Tabby", "Sassy", "Libby"];
+	let kitties = [];
 
-	function NewCat(name, i) {
+	function NewCat(name) {
 		this.name = name;
-		this.url = "images/cat-0" + i + ".jpg";
+		this.url = "images/" + name + ".jpg";
 	};
 
-	function addEvtLst() {
-		for (let catImage of catImages) {
-			catImage.addEventListener("click", countClick);
-			catImage.clicked = 0;
-		};
-	};
 
-	function countClick() {
-		this.clicked += 1;
-		document.querySelector("span." + this.className).innerHTML = (this.clicked);
-	};
+	for (let cat of cats) {
+		let kitty = new NewCat(cat);
+		kitties.push(kitty);
+	}
 
-	let cat1 = new NewCat("Tiffy", 1);
-	cats.push(cat1);
-	let cat2 = new NewCat("Fluffy", 2);
-	cats.push(cat2);
+	// function addEvtLst() {
+	// 	for (let catImage of catImages) {
+	// 		catImage.addEventListener("click", countClick);
+	// 		catImage.clicked = 0;
+	// 	};
+	// };
+	//
+	// function countClick() {
+	// 	this.clicked += 1;
+	// 	document.querySelector("span." + this.className).innerHTML = (this.clicked);
+	// };
 
-	for (i = 0; i < 2; i++) {
-		counter[i].className = cats[i].name;
-		catImages[i].className = cats[i].name;
-		catImages[i].src = cats[i].url;
-	};
+	// let cat1 = new NewCat("Tiffy", 1);
+	// cats.push(cat1);
+	// let cat2 = new NewCat("Fluffy", 2);
+	// cats.push(cat2);
+	//
+	// for (i = 0; i < 2; i++) {
+	// 	counter[i].className = cats[i].name;
+	// 	catImages[i].className = cats[i].name;
+	// 	catImages[i].src = cats[i].url;
+	// };
 
-	addEvtLst();
+	// addEvtLst();
+
+
 
 });
