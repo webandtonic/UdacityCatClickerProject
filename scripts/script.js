710d7e1600b9f5f5c28f0b533a9fd4b79ac21a04
Kerstin Hofmann <kerstinhofmann@Kerstins-iMac.fritz.box>
2019-02-08 14:22:39 +0100

    update to CCP: created cat display

--- scripts/script.js
+++ scripts/script.js
@@ -4,48 +4,98 @@ document.addEventListener("DOMContentLoaded", function () {
 	let model = {
 		cats: ["Tiffy", "Fluffy", "Tabby", "Sassy", "Libby"],
 		catObjects: [],
+		currentCatObject: null,
 		NewCat: function(name) {
 			this.name = name;
 			this.url = "images/" + name + ".jpg";
 			this.clicked = 0;
-		}
+		},
+		createCatObjects: function() {
+			for (let cat of model.cats) {
+				let catObj = new this.NewCat(cat);
+				model.catObjects.push(catObj);
+
+			};
+		},
 	};
 
 	let octopus = {
-		createCatObjects: function() {
-			for (let cat of model.cats) {
-				let catObj = new model.NewCat(cat);
-				model.catObjects.push(catObj);
-			};
+
+		init: function() {
+			model.createCatObjects();
+			model.currentCatObject = model.catObjects[0];
+
+			viewCatList.init();
+			viewCatDisplay.init();
 		},
 
-		createCatList: function() {
-			for (let cat of model.cats) {
-				viewCatList.createCatLink(cat);
-			};
+		getCatObjects: function() {
+			return model.catObjects;
+		},
+
+		getCurrentCatObject: function() {
+			return model.currentCatObject;
+		},
+
+		setCurrentCatObject: function(obj) {
+			model.currentCatObject = obj;
+		},
+
+		countClick: function(obj) {
+			obj.clicked += 1;
 		}
+
 	};
 
 	let viewCatList = {
-		catList: document.querySelector(".select-cat"),
-		createCatLink: function(cat) {
-			let catLink = document.createElement("a");
-			catLink.setAttribute("href", "#");
-			catLink.setAttribute("class", cat);
-			catLink.innerHTML = (cat);
-			viewCatList.catList.appendChild(catLink);
-			// catLink.addEventListener("click", displayCat);
+
+		init: function() {
+		 this.catList = document.querySelector(".select-cat");
+
+		 this.render();
+		},
+
+		render: function() {
+			let cats = octopus.getCatObjects();
+
+			for (let cat of cats) {
+				let catLink = document.createElement("button");
+				catLink.innerHTML = (cat.name);
+
+				catLink.addEventListener("click", (function(obj) {
+					return function() {
+						octopus.setCurrentCatObject(obj);
+						viewCatDisplay.render();
+					};
+				})(cat));
+
+				this.catList.appendChild(catLink);
+			};
 		}
+
 	};
 
 	let viewCatDisplay = {
-		// displayCat: function() {
-		//
-		// }
+
+		init: function() {
+			this.catHeading = document.querySelector(".cat-heading"),
+			this.catImage = document.querySelector(".cat-image"),
+			this.catName = document.querySelector(".cat-name"),
+			this.catCount = document.querySelector(".cat-count")
+
+		},
+
+		render: function() {
+			let cat = octopus.getCurrentCatObject();
+			this.catHeading.innerHTML = (cat.name);
+			this.catImage.src = (cat.url);
+			this.catImage.alt = (cat.name);
+			this.catName.innerHTML = (cat.name);
+			this.catCount.innerHTML = (cat.clicked);
+		}
 	};
 
-octopus.createCatObjects();
-octopus.createCatList();
-console.log(model.catObjects);
 
+
+	octopus.init();
 });
